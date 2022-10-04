fe47d0d254096a82b47f215e0af57b946fc558d2
Lee Jihoon <jihoolee@student.42seoul.kr>
2022-10-04 23:17:21 +0900

    [ADD]: html loading

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'view_count',
+        'created_at',
+        'updated_at',)
+    search_fields = ('title',)
+    #list_filter 이런 애도 있음
+    # list_display_links
--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    view_count = models.IntegerField(default=0)
 
+    created_at = models.DateTimeField(auto_now_add=True) # 생성시 시간 넎어주기
+    updated_at = models.DateTimeField(auto_now=True) # 생성 혹은 수정시 시간 넣어주기
